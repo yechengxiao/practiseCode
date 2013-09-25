e79186a96b3b9623a0bf1c401e00694afd0ea8b5
yechengxiao <yechengxiao@gmail.com>
2013-09-25 13:00:57 +0800

    add ThinkingInJava U5 Foreign.java PackagedClass.java

---
+++ ThinkingInJava/U5/src/foreign/Foreign.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: yechengxiao
+ * Date: 13-9-12
+ * Time: 下午10:39
+ * To change this template use File | Settings | File Templates.
+ * 5.6 练习4
+ * 访问控制，class、成员变量、成员方法都受控制
+ */
+package foreign;
+
+public class Foreign {
+    public static void main(String[] args) {
+        //PackagedClass p = new PackagedClass();
+        //PackagedClass为public class即可正常访问
+    }
+}
